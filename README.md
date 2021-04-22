101d3693676ee3a84d949db6adbedda1a38e4833
Steven Eisinger <steven.a.eisinger@gmail.com>
2021-04-22 14:32:42 +0200

    Added LICENSE and cleaned install

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dotfiles
+
 Personal dotfiles for *nix
+
+## Installation
+
+Ensure `zsh` and `git` are installed and on the path.
+
+```bash
+cd
+git clone git@github.com:Shtaiven/dotfiles.git .dotfiles
+cd .dotfiles
+zsh install.zsh
+```
--- install.zsh
+++ install.zsh
@@ -1,14 +1,30 @@
 #!/usr/bin/env zsh
+# Installs dotfiles to home directory.
+#
+# Authors:
+#   Steven Eisinger
+#
 
-# current script dir
+# vars
 INSTALL_DIR="${ZDOTDIR:-$HOME}"
 SOURCE_DIR="$0:A:h"
+ECHO="echo"
+LN_OPTS="-sf"
+
+setopt EXTENDED_GLOB
+
+# install prezto if it doesn't exist
+if [[ ! -d "$INSTALL_DIR/.zprezto" ]]; then
+    $ECHO "Installing prezto to $INSTALL_DIR/.zprezto"
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "$INSTALL_DIR/.zprezto"
+fi
 
 # install zsh dotfiles and prezto config
-setopt EXTENDED_GLOB
 for rcfile in $SOURCE_DIR/runcoms/^README.md(.N); do
-    ln -sf "$rcfile" "$INSTALL_DIR/.${rcfile:t}"
+    $ECHO "Installing $rcfile to $INSTALL_DIR/.${rcfile:t}"
+    ln $LN_OPTS "$rcfile" "$INSTALL_DIR/.${rcfile:t}"
 done
 
 # install powerlevel10k config
-ln -sf $SOURCE_DIR/p10k.zsh $INSTALL_DIR/.p10k.zsh
+$ECHO "Installing $SOURCE_DIR/p10k.zsh to $INSTALL_DIR/.p10k.zsh"
+ln $LN_OPTS "$SOURCE_DIR/p10k.zsh" "$INSTALL_DIR/.p10k.zsh"
