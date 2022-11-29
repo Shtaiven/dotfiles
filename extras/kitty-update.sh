82a611c8840f53437a04c365b68c53ed26030199
Steven Eisinger <seisinger@smart-robotics.nl>
2022-11-29 16:59:22 +0100

    added kitty-update script

---
+++ extras/kitty-update.sh
@@ -0,0 +1,12 @@
+# Install pre-built binary of kitty
+curl -Ls https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+# your system-wide PATH)
+ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/ 2>/dev/null
+
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+
+# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
