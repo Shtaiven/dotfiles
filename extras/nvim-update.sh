2c6d4929ae454eab41115180c22a47edb2953727
Steven Eisinger <seisinger@smart-robotics.nl>
2022-12-24 18:00:48 +0100

    feat: added nvim configuration

---
+++ extras/nvim-update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# Install pre-built binary of nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+
+# Move and rename nvim to PATH (assuming ~/.local/bin is in
+# your system-wide PATH)
+mv -f nvim.appimage ~/.local/bin/nvim
+
+# Give it executable permission
+chmod +x ~/.local/bin/nvim
+
