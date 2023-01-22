55bc257b5dbbdf7b7a5fb528725dac2003c1e4cd
Steven Eisinger <steven.a.eisinger@gmail.com>
2023-01-22 02:20:08 +0100

    fix: changed nvim installation to not use AppImage

--- scripts/installers/nvim.sh
+++ scripts/installers/nvim.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
-# Install pre-built binary of nvim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+
+DOWNLOAD_DEST="${HOME}/.local"
+INSTALL_DEST="${HOME}/.local/bin"
+TARBALL_NAME="nvim-linux64"
+TARBALL_EXT=".tar.gz"
+TAR_ARGS="xzvf"
+BIN_PATH="${TARBALL_NAME}/bin/nvim"
+INSTALL_NAME="nvim"
 
 # Make sure that ~/.local/bin exists
-mkdir -p ~/.local/bin
+mkdir -p ${DOWNLOAD_DEST}
+mkdir -p ${INSTALL_DEST}
 
-# Move and rename nvim to PATH (assuming ~/.local/bin is in
-# your system-wide PATH)
-mv -f nvim.appimage ~/.local/bin/nvim
+# Install pre-built binary of nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/${TARBALL_NAME}${TARBALL_EXT}
+
+# Extract and untar, move to the specified download folder, and remove the download
+tar ${TAR_ARGS} ${TARBALL_NAME}${TARBALL_EXT}
+mv -f ${TARBALL_NAME} ${DOWNLOAD_DEST}/${TARBALL_NAME}
+rm -rf ${TARBALL_NAME}${TARBALL_EXT}
+
+# Link the file into the specified install folder
+ln -sf ${DOWNLOAD_DEST}/${BIN_PATH} ${INSTALL_DEST}/${INSTALL_NAME}
 
 # Give it executable permission
-chmod +x ~/.local/bin/nvim
+chmod +x ${INSTALL_DEST}/${INSTALL_NAME}
 
