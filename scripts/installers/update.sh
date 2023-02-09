400aa0dc497761f50cdd6bbf3326a3580aa5628e
Steven Eisinger <seisinger@smart-robotics.nl>
2023-02-09 17:45:34 +0100

    feat: add an update script

---
+++ scripts/installers/update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Update the installed system
+
+SCRIPT_DIR=$(dirname "$0")
+SCRIPT_ARR=(
+    "install-webi-packages.sh"
+    "nvim.sh"
+    "kitty.sh"
+)
+
+for script in ${SCRIPT_ARR[@]}; do
+    source ${SCRIPT_DIR}/${script}
+done
+
