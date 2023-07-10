c70aa9a8fda5b573e5ea1fbf8506406fe6f4130d
Steven Eisinger <steven.a.eisinger@gmail.com>
2023-07-10 19:36:41 +0200

    feat: add colors for fancy tab bar (but don't enable)

--- stow/wezterm/.config/wezterm/wezterm.lua
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local config = {}
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.color_scheme = 'Gruvbox Material Dark'
 config.default_cursor_style = 'SteadyBar'
+config.allow_square_glyphs_to_overflow_width = "Always"
 
 config.window_padding = {
   left = '0cell',
@@ -13,8 +14,33 @@ config.window_padding = {
   bottom = '0cell',
 }
 
+config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'Fira Sans', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 10.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#101010',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#101010',
+}
+
 config.colors = {
   tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = 'none',
+
     -- The color of the strip that goes along the top of the window
     -- (does not apply when fancy tab bar is in use)
     background = '#101010',
@@ -64,5 +90,11 @@ config.colors = {
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+
+-- Triangle in the upper right used for terminal tabs 
+--local UPPER_RIGHT_TRIANGLE = wezterm.nerdfonts.ple_upper_right_triangle
+
+-- Triangle in the upper left used for terminal tabs
+--local UPPER_LEFT_TRIANGLE = wezterm.nerdfonts.ple_upper_left_triangle
 
 return config
