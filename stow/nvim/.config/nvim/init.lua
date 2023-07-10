b967ad2b959aa996bd96c4464bcf8ac953611fb6
Steven Eisinger <steven.a.eisinger@gmail.com>
2023-07-10 22:18:19 +0200

    feat: add more language servers

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -457,13 +457,16 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+  bashls = {},
+  clangd = {},
+  cmake = {},
+  jsonls = {},
+  ruff_lsp = {},
+  rust_analyzer = {},
+  taplo = {},
+  yamlls = {},
 
-  sumneko_lua = {
+  lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
