e0f04d861995b6879457ec58f9156aef58af594b
Steven Eisinger <steven.a.eisinger@gmail.com>
2023-07-10 20:21:00 +0200

    feat: nice bottom tabs for wezterm

--- stow/wezterm/.config/wezterm/wezterm.lua
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -92,9 +92,61 @@ config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 
 -- Triangle in the upper right used for terminal tabs 
---local UPPER_RIGHT_TRIANGLE = wezterm.nerdfonts.ple_upper_right_triangle
+local UPPER_RIGHT_TRIANGLE = wezterm.nerdfonts.ple_upper_right_triangle
 
 -- Triangle in the upper left used for terminal tabs
---local UPPER_LEFT_TRIANGLE = wezterm.nerdfonts.ple_upper_left_triangle
+local UPPER_LEFT_TRIANGLE = wezterm.nerdfonts.ple_upper_left_triangle
 
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+local function tab_title(tab_info)
+  local title = tab_info.tab_title
+  local index = tab_info.tab_index+1
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return index .. ':' .. title
+  end
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return index .. ":" .. tab_info.active_pane.title
+end
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local edge_background = '#101010'
+    local background = '#101010'
+    local foreground = '#5a524c'
+
+    if tab.is_active then
+      background = '#282828'
+      foreground = '#d4be98'
+    elseif hover then
+      background = '#101010'
+      foreground = '#928374'
+    end
+
+    local edge_foreground = background
+
+    local title = tab_title(tab)
+
+    -- ensure that the titles fit in the available space,
+    -- and that we have room for the edges.
+    title = wezterm.truncate_right(title, max_width - 2)
+
+    return {
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = UPPER_RIGHT_TRIANGLE },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = title },
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = UPPER_LEFT_TRIANGLE },
+    }
+  end
+)
 return config
