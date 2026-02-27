db6101c0d78757e94a32d0d846511748473d6afb
Steven Eisinger <seisinger@smart-robotics.nl>
2026-02-27 14:59:56 +0100

    feat: use telescope-file-browser instead of neo-tree

--- stow/nvim/.config/nvim/lua/plugins/telescope.lua
+++ stow/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -33,4 +33,38 @@ return {
       require('telescope').load_extension('fzf')
     end,
   },
+  {
+    'nvim-telescope/telescope-file-browser.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    keys = {
+      { '<leader>b', function() require('telescope').extensions.file_browser.file_browser() end, desc = 'Toggle File [B]rowser' },
+    },
+    config = function()
+      require('telescope').setup({
+        extensions = {
+          file_browser = {
+            hijack_netrw = true,
+            hidden = true,
+            respect_gitignore = false,
+          },
+        },
+      })
+      require('telescope').load_extension('file_browser')
+    end,
+    init = function()
+      vim.g.loaded_netrwPlugin = 1
+      vim.g.loaded_netrw = 1
+      vim.api.nvim_create_autocmd('BufEnter', {
+        callback = function(args)
+          local path = vim.api.nvim_buf_get_name(args.buf)
+          if path ~= '' and vim.fn.isdirectory(path) == 1 then
+            vim.schedule(function()
+              vim.cmd('bdelete ' .. args.buf)
+              require('telescope').extensions.file_browser.file_browser({ path = path })
+            end)
+          end
+        end,
+      })
+    end,
+  },
 }
--- stow/zsh/.zshrc
+++ stow/zsh/.zshrc
@@ -108,3 +108,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+
+# peon-ping quick controls
+alias peon="bash /home/steven/.claude/hooks/peon-ping/peon.sh"
+[ -f /home/steven/.claude/hooks/peon-ping/completions.bash ] && source /home/steven/.claude/hooks/peon-ping/completions.bash
